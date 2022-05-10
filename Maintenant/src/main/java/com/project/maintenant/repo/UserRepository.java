740b031b5a2794c9cda877c7c6ffb8e97bacdc08
Gaurav Kumar <gaurav2697@gmail.com>
2022-05-10 23:20:23 +0530

    Added repository annotation

--- Maintenant/src/main/java/com/project/maintenant/repo/UserRepository.java
+++ Maintenant/src/main/java/com/project/maintenant/repo/UserRepository.java
@@ -2,9 +2,12 @@ package com.project.maintenant.repo;
 
 import com.project.maintenant.model.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     List<UserEntity> getByUsername(String username);
