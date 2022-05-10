fd7ef8870da6bc24bc503d8741a54e774427daa3
Gaurav Kumar <gaurav2697@gmail.com>
2022-05-11 00:01:25 +0530

    edition in MaintenantApplication

--- Maintenant/src/main/java/com/project/maintenant/MaintenantApplication.java
+++ Maintenant/src/main/java/com/project/maintenant/MaintenantApplication.java
@@ -1,9 +1,16 @@
 package com.project.maintenant;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages="com.project.maintenant.repo")
+@EnableTransactionManagement
+@EntityScan(basePackages="com.project.maintenant.model.entities")
 public class MaintenantApplication {
 
     public static void main(String[] args) {
