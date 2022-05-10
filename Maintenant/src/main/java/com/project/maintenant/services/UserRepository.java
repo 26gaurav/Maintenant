37d14612246cbbfc13833efe1488c6f82f4f92da
Gaurav Kumar <gaurav2697@gmail.com>
2022-05-11 01:37:26 +0530

    repository annotation removed from UserRepository

--- Maintenant/src/main/java/com/project/maintenant/services/UserRepository.java
+++ Maintenant/src/main/java/com/project/maintenant/services/UserRepository.java
@@ -2,11 +2,9 @@ package com.project.maintenant.services;
 
 import com.project.maintenant.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
     List<UserEntity> getByUsername(String username);
