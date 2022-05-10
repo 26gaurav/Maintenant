fba734bea54e50e338a1fd32079bab880e53d943
Gaurav Kumar <gaurav2697@gmail.com>
2022-05-10 22:32:10 +0530

    application properties restored for docker

--- Maintenant/src/main/java/com/project/maintenant/MaintenantApplication.java
+++ Maintenant/src/main/java/com/project/maintenant/MaintenantApplication.java
@@ -3,8 +3,7 @@ package com.project.maintenant;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={
-        "com.project.maintenant.repo"})
+@SpringBootApplication
 public class MaintenantApplication {
 
     public static void main(String[] args) {
